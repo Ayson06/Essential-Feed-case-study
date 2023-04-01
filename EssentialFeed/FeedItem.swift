9f43cda20b6bec074f07974c34244eaf72f5242d
Ayson06 <sonia.ayadi06@gmail.com>
2023-04-02 01:00:26 +0400

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sonia AYADI on 02/04/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
