b6adad11c2c0dc7abf8767ebb2adea84b8836c97
Ayson06 <sonia.ayadi06@gmail.com>
2023-04-05 07:37:30 +0400

    guarantee that we cam load more than once

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -21,6 +21,15 @@ class RemoteFeedLoaderTests: XCTestCase {
         sut.load()
         XCTAssertEqual(client.requestedURL,url)
     }
+    
+    func test_loadTwice_requestsDataFromURL(){
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url:url)
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs,[url, url])
+    }
 
     //MARK: -Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -31,8 +40,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
+        
         func get(from url: URL){
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
